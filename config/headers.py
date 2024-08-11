0fc88ae2404594e06f9756feefc8879d8908a94a
Vladislav Kobzar <kobzarvllv@gmail.com>
2024-08-11 16:01:42 +0300

    Upload project

---
+++ config/headers.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class Headers:
+
+    basic = {
+        "Authorization": f"Bearer {os.getenv('API_TOKEN')}",
+        "X-Task-Id": "API-1"
+    }
